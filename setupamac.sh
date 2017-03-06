5700ab5c9f2f86ba5327a65f3df03dd89dc1cf9e
MoritzGruber <moritzgruber@yahoo.de>
2017-03-06 17:18:59 +0100

    added first version

---
+++ setupamac.sh
@@ -0,0 +1,17 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python
+brew install node
+brew install yarn
+npm install http-server -g
+npm install live-server -g
+npm install electron -g
+npm install bower -g
+yarn install vue-cli -g
+npm install gulp -g
+npm install -g cordova ionic
+brew cask install atom
+brew cask install google-chrome
+brew cask install iterm2
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/lysyi3m/osx-terminal-themes.git
+
