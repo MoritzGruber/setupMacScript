6c5aaedceaa729b799f29278cb1c508841c5e8c5
Moritz Gruber <m.gruber@cyclus.digital>
2020-02-03 14:46:17 +0100

    update

--- setupamac.sh
+++ setupamac.sh
@@ -3,6 +3,8 @@ brew install yarn
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 npm install next -g
 npm install plop -g
+brew tap sambadevi/powerlevel9k
+brew install powerlevel9k
 brew cask install iterm2
 brew cask install visual-studio-code
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
