238317796b9277b1c6d8484d5d7fbbc97f88cd7e
Moritz Gruber <m.gruber@cyclus.digital>
2020-02-03 14:43:47 +0100

    update

--- setupamac.sh
+++ setupamac.sh
@@ -1,16 +1,8 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python
 brew install node
 brew install yarn
-npm install http-server -g
-npm install live-server -g
-npm install electron -g
-npm install bower -g
-yarn install vue-cli -g
-npm install gulp -g
-npm install -g cordova ionic
-brew cask install atom
-brew cask install google-chrome
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+npm install next -g
+npm install plop -g
 brew cask install iterm2
 brew cask install visual-studio-code
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
